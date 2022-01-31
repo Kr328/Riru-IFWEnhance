2e0752050f51e34fdae8d8e7004498191b27268c
Kr328 <kr328app@outlook.com>
2022-01-31 14:38:11 +0800

    Chore: bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,8 +26,8 @@ subprojects {
     extensions.configure<BaseExtension> {
         val minSdkVersion = 26
         val targetSdkVersion = 31
-        val buildVersionName = "v12"
-        val buildVersionCode = 12
+        val buildVersionName = "v13"
+        val buildVersionCode = 13
 
         compileSdkVersion(targetSdkVersion)
 
