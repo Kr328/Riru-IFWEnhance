04071586f4c6180f52aa409df1d944bb151ea37a
Kr328 <kr328app@outlook.com>
2022-02-11 00:18:22 +0800

    Chore: update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,8 +9,8 @@ include(":module", ":hideapi")
 dependencyResolutionManagement {
     versionCatalogs {
         create("deps") {
-            val agp = "7.1.0"
-            val zloader = "1.8"
+            val agp = "7.1.1"
+            val zloader = "1.11"
             val magic = "1.4"
 
             alias("build-android").to("com.android.tools.build:gradle:$agp")
